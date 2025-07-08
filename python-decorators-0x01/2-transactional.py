d45b295ca35556f2e1f97aa5e37554e8b5f8a991
Adonyas Getachew <alx.adonias@gmail.com>
2025-07-08 15:23:25 -0700

    transaction managment decorator

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,36 @@
+import sqlite3 
+from functools import wraps
+
+def with_db_connection(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        with sqlite3.connect('decorator.db') as conn:
+            kwargs['conn'] = conn
+        return func(*args, **kwargs)
+    return wrapper
+
+def transactional(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = kwargs['conn']
+
+        if conn is None:
+            print('Database connection not established')
+        try:
+            result = func(*args, **kwargs)
+            conn.commit()
+            print('transction commited succesfully')
+            return result
+        except sqlite3.Error as e:
+            conn.rollback()
+            print(f'Calling ROLLBACK \nError: {e}')
+    return wrapper
+
+@with_db_connection 
+@transactional 
+def update_user_email(conn, user_id, new_email): 
+    cursor = conn.cursor() 
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id)) 
+#### Update user's email with automatic transaction handling 
+
+update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')

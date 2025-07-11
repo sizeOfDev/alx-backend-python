a3bcd4f574b5a21abf690d57dc8e3c0a1cd49f8a
Adonyas Getachew <alx.adonias@gmail.com>
2025-07-11 09:43:01 -0700

    added return

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -8,17 +8,18 @@ async def async_fetch_users():
             users = await cursor.fetchall()
             for user in users:
                 print(f"All Users: {user}" )
+            return users
         
 
 
 async def async_fetch_older_users():
-    async with aiosqlite.connect('decorator.db') as db:
+    async with aiosqlite.connect('users.db') as db:
         async with db.execute("SELECT * FROM users WHERE age > ?", (40,)) as cursor:
             users = await cursor.fetchall()
             for user in users:
                 print(f"Older Users: {user}")
+            return users
 
 async def fetch_concurrently(): await asyncio.gather( async_fetch_older_users(), async_fetch_users())
-
 
 asyncio.run(fetch_concurrently())

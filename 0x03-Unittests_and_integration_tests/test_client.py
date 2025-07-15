6994dd409bc75830e65358bbb5dc90ab3fbbecbf
Adonyas Getachew <alx.adonias@gmail.com>
2025-07-15 15:28:34 -0700

    addes test_org method

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+unittest module for client.py
+"""
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    Unittest for GithubOrgClient.
+    """
+
+    @parameterized.expand([
+        ("google",),
+        ("abc",)
+    ])
+    @patch("client.get_json")
+    def test_org(self, org_name, mock_get_json):
+        """Test org method"""
+        client = GithubOrgClient(org_name)
+        client.org()
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+        )
+        mock_get_json.return_value = {"repos_url":
+                                      "https://api.github.com/"
+                                      "orgs/google/repos"}

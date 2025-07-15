9ae22be86cfc20603626ee440cc10ff23948706e
Adonyas Getachew <alx.adonias@gmail.com>
2025-07-15 01:32:37 -0700

    test to raise exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -25,3 +25,11 @@ class TestAccessNestedMap(unittest.TestCase):
         """Run test case: {name}"""
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)

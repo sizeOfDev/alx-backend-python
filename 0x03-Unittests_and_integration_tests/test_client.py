1c385bf97f58a939a76638c91b106fd49b4d5f57
Adonyas Getachew <alx.adonias@gmail.com>
2025-07-16 06:44:00 -0700

    integeration test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,9 +5,10 @@ unittest module for client.py
 """
 
 import unittest
-from unittest.mock import patch, PropertyMock
-from parameterized import parameterized
+from unittest.mock import patch, PropertyMock, Mock
+from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -77,3 +78,42 @@ class TestGithubOrgClient(unittest.TestCase):
         """Test has_license static method"""
         result = GithubOrgClient.has_license(repo, license_key)
         self.assertEqual(result, expected)
+
+
+@parameterized_class(
+    ("org_payload", "repos_payload",
+     "expected_repos", "apache2_repos"), TEST_PAYLOAD)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """
+    Integeration test for GithubOrgClient class
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """Setup class"""
+        def sideEffect(url):
+            if url == cls.org_payload.get("repos_url"):
+                mock_response = Mock()
+                mock_response.json.return_value = cls.repos_payload
+                return mock_response
+
+            mock_response = Mock()
+            mock_response.json.return_value = cls.org_payload
+            return mock_response
+
+        cls.get_patcher = patch("requests.get", side_effect=sideEffect)
+        cls.mock_get = cls.get_patcher.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Tear down class"""
+        cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        """
+        Test the public_repos method
+        """
+
+        client = GithubOrgClient("google")
+        repos = client.public_repos()
+        self.assertEqual(repos, self.expected_repos)

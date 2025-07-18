1bc0141e0d0912d7c04b1f5c194be2f38e981af8
Adonyas Getachew <alx.adonias@gmail.com>
2025-07-18 04:32:22 -0700

    added fields in users model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -6,10 +6,14 @@ from django.contrib.auth.models import AbstractUser
 class users(AbstractUser):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(_('first_name'),max_length=255)
+    last_name = models.CharField(_('last_name'),max_length=255)
+    password = models.CharField(_('password'), max_length=128)
     birthdate = models.DateField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pic/', null=True, blank=True)
-    bio = models.TextField(blank=True, null=True)
     last_seen = models.DateTimeField(default=timezone.now())
+
+    
 
     def __str__(self):
         return self.username
@@ -26,8 +30,8 @@ class Message(models.Model):
     message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
     sender = models.ForeignKey(users, on_delete=models.CASCADE, related_name='sent_messages')
-    text = models.TextField()
-    timestamp = models.DateTimeField(default=timezone.now())
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(default=timezone.now())
     read = models.BooleanField(default=False)
 
     def __str__(self):

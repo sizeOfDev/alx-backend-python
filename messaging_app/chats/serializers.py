ac5c8f643d48cb07f77d523d5a87c832117c07e5
Adonyas Getachew <alx.adonias@gmail.com>
2025-07-18 05:17:41 -0700

    add ConversaionCreateSerializer and UserRegisterSerialize for read and write separation

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -3,18 +3,34 @@ from .models import users, Conversation, Message
 from django.contrib.auth.password_validation import validate_password
 
 class UsersSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     class Meta:
         model = users
-        fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'birthdate', 'password']
+        fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'birthdate']
         read_only_fields = ['user_id']
-        
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+
+    class Meta:
+        model = users
+        fields = ['user_id', 'username', 'email', 'password', 'first_name', 'last_name']
+        read_only_fields = ['user_id']
+
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = super().create(validated_data)
-        user.set_password(password)
+        user = users(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])
         user.save()
-        return user 
+        return user
+
+    def validate_email(self, value):
+        if users.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email is already in use.")
+        return value 
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UsersSerializer(read_only=True)
@@ -26,7 +42,31 @@ class MessageSerializer(serializers.ModelSerializer):
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UsersSerializer(many=True, read_only=True)
     messages = MessageSerializer(many=True, read_only=True)
+    last_message = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
         fields = ['conversation_id', 'participants', 'created_at', 'messages']
+
+    def get_last_message(self, obj):
+        last = obj.messages.order_by('-sent_at').first()
+        if last:
+            return MessageSerializer(last).data
+        return None
+    
+class ConversationCreateSerializer(serializers.ModelSerializer):
+    participants = serializers.PrimaryKeyRelatedField(queryset=users.objects.all(), many=True)
+
+    class Meta:
+        model = Conversation
+        fields = ['participants']
+
+    def validate_participants(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("A conversation must have at least two participants.")
+        return value
+
+    def create(self, validated_data):
+        conversation = Conversation.objects.create()
+        conversation.participants.set(validated_data['participants'])
+        return conversation

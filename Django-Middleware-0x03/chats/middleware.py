41ed6568edc096659397a7ccc05b4ad638ab6f7e
Adonyas Getachew <alx.adonias@gmail.com>
2025-07-27 06:14:36 -0700

    Restrict by chat accesstime middleware

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from django.http import HttpResponseForbidden
+
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -7,7 +9,7 @@ class RequestLoggingMiddleware:
     def __call__(self, request):
         user = request.user if request.user.is_authenticated else 'Anonymous'
 
-        with open('./chats/requests.log', 'a') as log_file:
+        with open('requests.log', 'a') as log_file:
             log_file.write(f"{datetime.now()} - User: {user} - Path: {request.path}\n")
             
         # print(f"{datetime.now()} - User: {user} - Path: {request.path}")
@@ -15,3 +17,26 @@ class RequestLoggingMiddleware:
         resposnse = self.get_response(request)
 
         return resposnse
+    
+class RestrictAccessByTimeMiddleware:
+
+    def __init__(self, get_response):
+
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        user = request.user if request.user.is_authenticated else 'Anonymous'
+        
+        current_time = datetime.now().time()
+
+
+        start_time = datetime.strptime("06:00", "%H:%M").time()
+        end_time = datetime.strptime("21:00", "%H:%M").time()
+
+        if not (start_time <= current_time <= end_time):
+            return HttpResponseForbidden("Access restricted ( 6PM - 9PM)", status=403)
+
+        response = self.get_response(request)
+        
+        return response

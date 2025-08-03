bc1667f405f47340fc4860ad3fa496b99b3a6a57
Adonyas Getachew <alx.adonias@gmail.com>
2025-08-03 13:38:22 -0700

    add cleanup user data using post delete

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -7,6 +7,7 @@ class Message(models.Model):
     receiver = models.CharField(max_length=250)
     content = models.CharField(max_length=500)
     timestamp = models.DateTimeField(default=datetime.now())
+    edited = models.BooleanField(default=False)
 
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -16,3 +17,9 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"Notification for {self.user} - Read: {self.read}"
+
+class  MessageHistory(models.Model):
+
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    old_content = models.CharField(max_length=500)
+    timestamp = models.DateTimeField(default=datetime.now())
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,7 +1,7 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from models import Message, Notification
+from models import Message, Notification, MessageHistory
 
 
 @receiver(post_save, sender=Message)
@@ -20,3 +20,32 @@ def notification_saved(sender, instance, created, **kwargs):
         print(f"Notification for {instance.user} regarding message {instance.message.id} has been saved.")
     else:
         print(f"Notification for {instance.user} regarding message {instance.message.id} has been updated.")
+
+
+
+@receiver(pre_save, sender=Message)
+def log_message_history(sender, instance, **kwargs):
+    if instance.pk:
+        old_message = Message.objects.get(pk=instance.pk)
+        if old_message.content != instance.content:
+            MessageHistory.objects.create(
+                message=old_message,
+                old_content=old_message.content,
+                timestamp=old_message.timestamp
+            )
+            print(f"Message history logged for message {instance.pk}. Old content: {old_message.content}")
+        else:
+            print(f"No change in content for message {instance.pk}. No history logged.")
+    else:
+        print("Creating a new message, no history to log.") 
+
+@receiver(post_delete, sender=User)
+def cleanup_user_data(sender, instance, **kwargs):
+    
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+
+    Notification.objects.filter(user=instance).delete()
+
+    for message in Message.objects.filter(sender=instance) | Message.objects.filter(receiver=instance):
+        MessageHistory.objects.filter(message=message).delete()
